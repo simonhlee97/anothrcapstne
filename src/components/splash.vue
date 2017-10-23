3dac1f276dbfbaff3f2d0680d0f74c5ed8b88ab6
Simon Lee <simonhlee97@hotmail.com>
2017-10-22 22:26:29 -0400

    added some vuetify to forms

--- src/components/splash.vue
+++ src/components/splash.vue
@@ -1,21 +1,24 @@
 <template>
 
 	<div id="about">
-		<h1>About SportsPress</h1>
-		<p>This Vue.js and Firebase project was my 'Capstone Project' for Eleven Fifty, created October 2017.</p>
-		<p><a href="https://github.com/simonhlee97/anothrcapstne/" target="_blank">GitHub Repo</a></p>
-		<p>Created by: <a href="https://www.linkedin.com/in/simon-h-lee/" target="_blank">Simon Lee</a></p>
+		<h3>About SportsPress</h3>
+		<ul>
+			<li>This Vue.js and Firebase project was my 'Capstone Project' for Eleven Fifty, created October 2017.</li>
+			<li><a href="https://github.com/simonhlee97/anothrcapstne/" target="_blank">GitHub Repo</a></li>
+			<li>Created by: <a href="https://www.linkedin.com/in/simon-h-lee/" target="_blank">Simon Lee</a></li>
+		</ul>
 		<hr>
-		<h3>Updates</h3>
+		<h4>Updates</h4>
 		<p>10/22/17: new background photo, created 'About SportsPress' Component</p>
+		<p>10/6/17: version 1.0 deployed to firebase</p>
 		
 		<!-- todos: Sticky Nav; Footer; Vuetify -->
 
 		<hr>
-		<h3>Credits</h3>
+		<h4>Credits</h4>
 		<p>Background Photo by: <a href="https://unsplash.com/photos/aMuXhFkbxEw" target="_blank">Joshua Peacock at Unsplash</a></p>
 		<hr>
-		<h3>Build Tools</h3>
+		<h4>Build Tools</h4>
 		<ul>
 			<li>Vue.js 2</li>
 			<li>vue-router</li>
@@ -33,11 +36,12 @@ export default{
 <style scoped>
 #about {
 	background-color: navy;
-	width: 55%;
+	width: 65%;
 	max-height: 55%;
 	margin: 8em auto;
 	color: white;
 	padding: 3em;
+	border-radius: 9px;
 	
 }
 </style>
